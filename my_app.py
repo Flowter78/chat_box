fde84526226f84b689a068ad19027013a931ca20
Florent Ollivier <florentollivier78@gmail.com>
2025-04-06 23:41:57 -0400

    ras

--- my_app.py
+++ my_app.py
@@ -4,9 +4,10 @@ import os
 import openai
 
 
-# Charge les variables d'environnement depuis .env 
+# Charge les variables d'environnement depuis .env
 load_dotenv()
-print("CLE CHARGÉE :", os.getenv("OPENAI_API_KEY"))
+
+#print("CLE CHARGÉE :", os.getenv("OPENAI_API_KEY"))
 
 # Récupère la clé
 openai.api_key = os.getenv("OPENAI_API_KEY")
